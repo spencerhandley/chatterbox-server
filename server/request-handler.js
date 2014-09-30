83af3c847fb9306695be800f0fe89d2324fd3d86
spencer48 <spencerhandley@gmail.com>
2014-09-30 11:13:29 -0700

    Server static assets with server

--- server/request-handler.js
+++ server/request-handler.js
@@ -5,12 +5,15 @@
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
 var url = require('url');
+var fs = require('fs');
 var http = require('http');
+var path = require('path');
+var clientPath = '../client';
 var controller  = require('./controller');
 var results = [];
-results.push({text: 'yolotrollo', username: 'sharkbait', createdAt: new Date(), roomname: "lobby"});
-results.push({text: '#burningman', username: 'kobra', createdAt: new Date(), roomname: "lobby"});
-results.push({text: 'date night with my hubby, tacobell! ;) #blessed', username: 'magee', createdAt: new Date(), roomname: "lobby"});
+results.push({text: 'yolotrollo', username: 'sharkbait', createdAt: new Date(), roomname: 'lobby'});
+results.push({text: '#burningman', username: 'kobra', createdAt: new Date(), roomname: 'lobby'});
+results.push({text: 'date night with my hubby, tacobell! ;) #blessed', username: 'magee', createdAt: new Date(), roomname: 'lobby'});
 var routes = {
   classes: {
     'GET': controller.get,
@@ -19,34 +22,75 @@ var routes = {
   }
 };
 
+
 exports.handleRequest = function(request, response) {
   var parsedUrl = url.parse(request.url).pathname.split('/');
   var statusCode;
 
-  var return404 = function(){
-    statusCode = 404;
-    response.writeHead(statusCode, headers);
-    response.end();
-    return;
-  };
+  if(url.parse(request.url).pathname === '/'){
+    console.log("serving index.html")
+    serveStaticFile(clientPath + '/index.html', request, response);
+  } else if (parsedUrl[parsedUrl.length-1].split('.').length > 1) {
+    console.log("serving other static file");
+    console.log(parsedUrl.join('/'), 'path requested');
+    serveStaticFile(clientPath + parsedUrl.join('/'), request, response);
+  } else {
+    var return404 = function(){
+      statusCode = 404;
+      response.writeHead(statusCode, headers);
+      response.end();
+      return;
+    };
 
-  if (routes[parsedUrl[1]] === undefined){
-    return return404();
+    if (routes[parsedUrl[1]] === undefined){
+      return return404();
+    }
+
+    var controllerMethod = routes[parsedUrl[1]][request.method];
+
+    console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+    var headers = defaultCorsHeaders;
+
+    if (controllerMethod === undefined){
+      return return404();
+    }
+
+    controllerMethod(request, response, parsedUrl, headers, results);
   }
-
-  var controllerMethod = routes[parsedUrl[1]][request.method];
-
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  var headers = defaultCorsHeaders;
-
-  if (controllerMethod === undefined){
-    return return404();
-  }
-
-  controllerMethod(request, response, parsedUrl, headers, results);
 };
 
+var serveStaticFile = function(pathName, req, res){
+  console.log(pathName);
+  var splitPathname = pathName.split('/');
+  var fileNameSplit = splitPathname[splitPathname.length-1].split('.');
+  var extension = fileNameSplit[fileNameSplit.length - 1];
+  var contentType = null;
+  switch (extension){
+    case 'html':
+      contentType = 'text/html';
+      break;
+    case 'js':
+      contentType = 'application/javascript';
+      break;
+    case 'css':
+      contentType = 'text/css';
+      break;
+  }
+  var absolutePathname = path.join(__dirname, pathName);
+  fs.exists(absolutePathname, function(exists){
+    if(!exists){
+      absolutePathname = path.join(__dirname, '../client/404.html');
+    }
+    console.log()
+    fs.readFile(absolutePathname, function(err, data){
+      console.log(data)
+      res.writeHead(200, {'Content-Type': contentType});
+      res.write(data + '');
+      res.end();
+    });
+  });
+};
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
  * This CRUCIAL code allows this server to talk to websites that
